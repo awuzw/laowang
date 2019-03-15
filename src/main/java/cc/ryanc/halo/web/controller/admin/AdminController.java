4acf5c5427077e77c5edf5be5b1eecfe6d414cc7
awuzw <1612076097@qq.com>
2019-03-15 16:11:55 +0800

    测试git

--- src/main/java/cc/ryanc/halo/web/controller/admin/AdminController.java
+++ src/main/java/cc/ryanc/halo/web/controller/admin/AdminController.java
@@ -348,11 +348,9 @@ public class AdminController extends BaseController {
             post.setPostUpdate(new Date());
         }
         postService.create(post);
-        System.out.println(".....");
+
         return new JsonResult(ResultCodeEnum.SUCCESS.getCode());
     }
 
-    public void test(){
-        System.out.println("你好");
-    }
+
 }
