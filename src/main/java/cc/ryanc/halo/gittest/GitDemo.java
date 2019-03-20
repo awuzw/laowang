3ddb050bbb94e850e10c74d32fdf080c360ae382
awuzw <1612076097@qq.com>
2019-03-20 16:35:21 +0800

    测试git

---
+++ src/main/java/cc/ryanc/halo/gittest/GitDemo.java
@@ -0,0 +1,13 @@
+package cc.ryanc.halo.gittest;
+
+/**
+ * @author ：mmzs
+ * @date ：Created in 2019/3/20 16:33
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class GitDemo {
+    private String name;
+    private Integer id;
+}
